f4f7c4f284a00c7a1e28ff7f03b6833ca78c8683
EMULE2026 <liza.pc2@gmail.com>
2026-01-23 14:48:30 +0000

    sf-govern AI

---
+++ SnoML/e2e_ml_demo.sql
@@ -0,0 +1,92 @@
+HOL_DB.PUBLIC."sfguide-getting-started-machine-learning"HOL_DB.PUBLIC."sfguide-getting-started-machine-learning"HOL_DB.PUBLIC."sfguide-getting-started-machine-learning"-- Using ACCOUNTADMIN, create a new role for this exercise 
+USE ROLE ACCOUNTADMIN;
+SET USERNAME = (SELECT CURRENT_USER());
+SET ALLOW_EXTERNAL_ACCESS_FOR_TRIAL_ACCOUNTS = TRUE;
+CREATE OR REPLACE ROLE E2E_SNOW_MLOPS_ROLE;
+
+-- Grant necessary permissions to create databases, compute pools, and service endpoints to new role
+GRANT CREATE DATABASE on ACCOUNT to ROLE E2E_SNOW_MLOPS_ROLE; 
+GRANT CREATE COMPUTE POOL on ACCOUNT to ROLE E2E_SNOW_MLOPS_ROLE;
+GRANT CREATE WAREHOUSE ON ACCOUNT to ROLE E2E_SNOW_MLOPS_ROLE;
+GRANT BIND SERVICE ENDPOINT on ACCOUNT to ROLE E2E_SNOW_MLOPS_ROLE;
+
+-- grant new role to user and switch to that role
+GRANT ROLE E2E_SNOW_MLOPS_ROLE to USER identifier($USERNAME);
+USE ROLE E2E_SNOW_MLOPS_ROLE;
+
+-- Create warehouse
+-- CREATE OR REPLACE WAREHOUSE E2E_SNOW_MLOPS_WH WITH WAREHOUSE_SIZE='MEDIUM';
+
+-- Create Database 
+CREATE OR REPLACE DATABASE E2E_SNOW_MLOPS_DB;
+
+-- Create Schema
+CREATE OR REPLACE SCHEMA MLOPS_SCHEMA;
+
+-- Create compute pool
+CREATE COMPUTE POOL IF NOT EXISTS MLOPS_COMPUTE_POOL 
+ MIN_NODES = 1
+ MAX_NODES = 1
+ INSTANCE_FAMILY = CPU_X64_M;
+
+-- Using accountadmin, grant privilege to create network rules and integrations on newly created db
+USE ROLE ACCOUNTADMIN;
+-- GRANT CREATE NETWORK RULE on SCHEMA MLOPS_SCHEMA to ROLE E2E_SNOW_MLOPS_ROLE;
+GRANT CREATE INTEGRATION on ACCOUNT to ROLE E2E_SNOW_MLOPS_ROLE;
+USE ROLE E2E_SNOW_MLOPS_ROLE;
+
+-- Create an API integration with Github
+CREATE OR REPLACE API INTEGRATION GITHUB_INTEGRATION_E2E_SNOW_MLOPS
+   api_provider = git_https_api
+   api_allowed_prefixes = ('https://github.com/Snowflake-Labs')
+   enabled = true
+   comment='Git integration with Snowflake Demo Github Repository.';
+   
+-- Create the integration with the Github demo repository
+CREATE OR REPLACE GIT REPOSITORY GITHUB_REPO_E2E_SNOW_MLOPS
+   ORIGIN = 'https://github.com/Snowflake-Labs/sfguide-build-end-to-end-ml-workflow-in-snowflake' 
+   API_INTEGRATION = 'GITHUB_INTEGRATION_E2E_SNOW_MLOPS' 
+   COMMENT = 'Github Repository ';
+
+-- Fetch most recent files from Github repository
+ALTER GIT REPOSITORY GITHUB_REPO_E2E_SNOW_MLOPS FETCH;
+
+-- Copy notebook into snowflake configure runtime settings
+CREATE OR REPLACE NOTEBOOK E2E_SNOW_MLOPS_DB.MLOPS_SCHEMA.TRAIN_DEPLOY_MONITOR_ML
+FROM '@E2E_SNOW_MLOPS_DB.MLOPS_SCHEMA.GITHUB_REPO_E2E_SNOW_MLOPS/branches/main/' 
+MAIN_FILE = 'train_deploy_monitor_ML_in_snowflake.ipynb' QUERY_WAREHOUSE = COMPUTE_WH
+RUNTIME_NAME = 'SYSTEM$BASIC_RUNTIME' 
+COMPUTE_POOL = 'MLOPS_COMPUTE_POOL'
+IDLE_AUTO_SHUTDOWN_TIME_SECONDS = 3600;
+
+--DONE! Now you can access your newly created notebook with your E2E_SNOW_MLOPS_ROLE and run through the end-to-end workflow!
+
+SHOW NOTEBOOKS;
+
+GRANT USAGE ON DATABASE E2E_SNOW_MLOPS_DB to ROLE ACCOUNTADMIN;
+GRANT USAGE ON SCHEMA MLOPS_SCHEMA to ROLE ACCOUNTADMIN;
+
+GRANT CREATE TABLE ON SCHEMA E2E_SNOW_MLOPS_DB.MLOPS_SCHEMA TO ROLE ACCOUNTADMIN;
+
+-- Grant all privileges (if you own the schema)
+GRANT ALL PRIVILEGES ON SCHEMA E2E_SNOW_MLOPS_DB.MLOPS_SCHEMA TO ROLE ACCOUNTADMIN;
+
+USE ROLE ACCOUNTADMIN;
+SELECT
+FUNCTION_CATALOG,  -- same as database
+FUNCTION_SCHEMA,
+FUNCTION_NAME,
+FUNCTION_OWNER,
+FUNCTION_LANGUAGE,
+DATE(CREATED) AS DATE_CREATED,
+DATE(LAST_ALTERED) AS DATE_MODIFIED,
+RUNTIME_VERSION,
+-- FROM INFORMATION_SCHEMA.FUNCTIONS   -- single database
+FROM SNOWFLAKE.ACCOUNT_USAGE.FUNCTIONS   -- entire account
+WHERE FUNCTION_LANGUAGE = 'PYTHON'
+AND RUNTIME_VERSION = '3.11'
+ORDER BY 1, 2, 3;
+
+select * from information_schema.packages where language = 'python';
+
+SHOW GIT REPOSITORIES;
